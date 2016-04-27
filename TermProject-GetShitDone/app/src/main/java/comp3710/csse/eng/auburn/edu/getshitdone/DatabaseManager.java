e7789ac01bfb9617db08d2a57ecef73ee0d10320
Jacob <jacob.varner14@gmail.com>
2016-04-26 21:52:55 -0500

    Decided to remove due dates and due times for now. Also finished the method that gets tasks.

--- TermProject-GetShitDone/app/src/main/java/comp3710/csse/eng/auburn/edu/getshitdone/DatabaseManager.java
+++ TermProject-GetShitDone/app/src/main/java/comp3710/csse/eng/auburn/edu/getshitdone/DatabaseManager.java
@@ -24,7 +24,7 @@ public final class DatabaseManager {
         public void onCreate(SQLiteDatabase db) {
             db.execSQL("CREATE TABLE categories (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT);");
             db.execSQL("CREATE TABLE tasks (id INTEGER PRIMARY KEY AUTOINCREMENT , taskName TEXT," +
-                    " category TEXT, completed BOOLEAN, description TEXT, dueDate DATE, dueTime TIME);");
+                    " category TEXT, completed TEXT, description TEXT);");
         }
 
         public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
@@ -45,16 +45,13 @@ public final class DatabaseManager {
         db.insert("categories", "null", values);
     }
 
-    public static void addTask(String name, String category, String description, String dueDate, String dueTime) {
+    public static void addTask(String name, String category, String description) {
         SQLiteDatabase db = SQLiteDatabase.openDatabase("GetShitDone.db", null, SQLiteDatabase.OPEN_READWRITE);
         ContentValues values = new ContentValues();
         values.put("taskName", name);
         values.put("category", category);
-        values.put("completed", false);
-        values.put("deleted", false);
+        values.put("completed", "false");
         values.put("description", description);
-        values.put("dueDate", dueDate);
-        values.put("dueTime", dueTime);
         db.insert("tasks", "null", values);
     }
 
@@ -62,23 +59,37 @@ public final class DatabaseManager {
     public static ArrayList<String[]> getTasks(String category, int flag) {
         SQLiteDatabase db = SQLiteDatabase.openDatabase("GetShitDone.db", null, SQLiteDatabase.OPEN_READWRITE);
         String completed;
-        String[] projection = {"id", "taskName", "completed", "description", "dueDate", "dueTime"};
+        String[] projection = {"id", "taskName", "category", "completed", "description"};
         String selection = "category = ? AND flag = ?";
         if (flag == 1) {
-            completed = "0";
+            completed = "false";
         } else {
-            completed = "";
+            completed = "true || false";
         }
         String[] selectionArgs = {category, completed};
 
         Cursor c = db.query("tasks", projection, selection, selectionArgs, null, null, "ASC");
 
-        return null;
+        ArrayList<String[]> output = new ArrayList<>();
+
+        c.moveToFirst();
+        while(!c.isAfterLast()) {
+            String[] row = new String[c.getColumnCount()];
+            row[0] = c.getString(0); // id
+            row[1] = c.getString(1); // taskName
+            row[2] = c.getString(2); // category
+            row[3] = c.getString(3); // completed
+            row[4] = c.getString(4); // description
+            output.add(row);
+        }
+
+        return output;
     }
 
     public static ArrayList<String> getCategories() {
+        ArrayList<String> output = new ArrayList<>();
 
-        return null;
+        return output;
     }
 
     public static void markComplete(int taskId) {
